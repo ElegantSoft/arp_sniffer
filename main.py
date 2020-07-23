2c5d54a6bd781fb1a32a00cc6505e613c55ad90c
ElegantSoft <react.laravel.dev@gmail.com>
2020-07-23 18:31:48 -0400

    show package

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 
+import typing
 import scapy.all as scapy
+from scapy.layers import http
 
 
-def process_sniffed_packet(packet):
-    print(packet)
+def process_sniffed_packet(packet: scapy.Packet):
+    if packet.haslayer(http.HTTPRequest):
+        print(packet.show())
 
 
 def sniff(interface):
